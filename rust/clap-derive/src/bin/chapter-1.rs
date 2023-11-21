0c95ae355136f4af31528ef647bcb8bb207cd2ea
Rhys Parry <rhys@i-think22.net>
2023-11-21 14:03:11 +1000

    Enable the next line help

--- rust/clap-derive/src/bin/chapter-1.rs
+++ rust/clap-derive/src/bin/chapter-1.rs
@@ -2,9 +2,12 @@ use clap::Parser;
 
 #[derive(Parser)]
 #[command(author, version, about)] // Read the author, version, and about from Cargo.toml
+#[command(next_line_help = true)] // Display the help message on the next line
 struct Cli {
+    /// The value for the two
     #[arg(long)]
     two: String,
+    /// The value for the one
     #[arg(long)]
     one: String,
 }
