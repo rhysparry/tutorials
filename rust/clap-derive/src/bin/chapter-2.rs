48897b172c720ba60774954978c3a991a6f8d710
Rhys Parry <rhys@i-think22.net>
2023-11-21 14:14:35 +1000

    Use a switch flag for verbosity

--- rust/clap-derive/src/bin/chapter-2.rs
+++ rust/clap-derive/src/bin/chapter-2.rs
@@ -5,10 +5,14 @@ use clap::Parser;
 struct Cli {
     #[arg(short, long)]
     name: Vec<String>,
+
+    #[arg(short, long)]
+    verbose: bool,
 }
 
 fn main() {
     let cli = Cli::parse();
 
     println!("name: {:?}", cli.name);
+    println!("verbose: {:?}", cli.verbose)
 }
